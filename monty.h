327794176d427de908db6a571f7e7f93c3efcf5c
Bamzzzz <muyiwaayobami@yahoo.com>
2024-01-18 15:42:00 -0800

    FIFO LIFO

--- monty.h
+++ monty.h
@@ -9,17 +9,19 @@
 #include <ctype.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
- * @prev: points to the previous element of the stack
- * @next: points to the next element of the stack
- * description: doubly linked list node structure
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 
 /**
@@ -43,13 +45,15 @@ extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
- * @f: function
+ * @f: function to handle the opcode
+ *
  * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
